0ccbda982bbb81919176903cf9c046a9bcb294a8
Eric Rübl <eric@zario.go.ro>
2018-05-07 17:17:14 +0300

    Added some utils

---
+++ core/Model/Doctor.py
@@ -0,0 +1,3 @@
+class Doctor(object):
+    def __init__(self):
+        pass
---
+++ core/Utils/RequestStatus.py
@@ -0,0 +1,14 @@
+class RequestStatus(object):
+    to_string = {
+        0: "open",
+        1: "escalated",
+        2: "in_progress",
+        3: "closed"
+    }
+
+    to_code = {
+        "open" : 0,
+        "escalated": 1,
+        "in_progress": 2,
+        "closed": 3
+    }
