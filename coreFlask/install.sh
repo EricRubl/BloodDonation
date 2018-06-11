fb58158a815339d7bd6c7ecb47461aad73016f07
Eric Rübl <eric@zario.go.ro>
2018-06-11 17:05:38 +0300

    Added install script

---
+++ coreFlask/install.sh
@@ -0,0 +1,4 @@
+sudo apt install -y git python3 python3-pip
+sudo pip3 install Flask Flask-Login Jinja2 mysql-connector-python-rf requests
+git clone https://github.com/EricRubl/BloodDonation.git
+python3 BloodDonation/coreFlask/app.python3
